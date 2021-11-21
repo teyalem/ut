f1be133bc94671095a384e04de22fa12910d1df4
Hyeonung Baek <wiserlix@gmail.com>
2021-11-21 18:45:40 +0900

    rename IO functions

--- io.ml
+++ io.ml
@@ -1,13 +1,14 @@
-let rec read_lines (file: in_channel) : string list =
-  match input_line file with
-  | line -> line::(read_lines file)
+(* read lines from input channel. *)
+let rec input_lines (ic: in_channel) =
+  match input_line ic with
+  | line -> line::(input_lines ic)
   | exception End_of_file -> []
 
 (* NOTE: input_all function introduced in Ocaml 4.14. *)
-let input_all (file: in_channel) : string =
+let input_all (file: in_channel) =
   really_input_string file (in_channel_length file)
 
-let read_data () =
+let read_all () =
   Seq.unfold (fun () -> try Some (read_line (), ()) with _ -> None) ()
   |> List.of_seq
   |> String.concat "\n"
