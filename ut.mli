6d6bb1049fc198033f0db8111e3b624f780d7a2d
Hyeonung Baek <wiserlix@gmail.com>
2021-11-26 15:38:01 +0900

    Documentize ut.mli

--- ut.mli
+++ ut.mli
@@ -1,39 +1,63 @@
-(* take n elements from list. *)
+(** Utilities for Problem-Solving *)
+
+(** [take n xs] takes n elements from xs. *)
 val take : int -> 'a list -> 'a list
 
-(* reverse array. *)
+(** [rev_array arr] returns reverse array. obsolete in 4.14 *)
 val rev_array : 'a array -> 'a array
 
-(* return sum of all elements in list. *)
+(** [sum ns] returns sum of all elements in ns. *)
 val sum : int list -> int
 
-(* starts_with pat str checks that string str starts with pattern pat. *)
+(** [starts_with pat str] checks that string str starts with pattern pat. *)
 val starts_with : string -> string -> bool
 
-(* group and count same elements *)
+(** [group_count xs] returns (element, number) list, where element is in
+ * xs and number is the number of the element in xs. List must be
+ * sorted beforehand. *)
 val group_count: 'a list -> ('a * int) list
 
-(* Common modules *)
-module IO = Io (* Useful IO Operations *)
-module Range = Range
-module Delim = Delim
-module Coord = Coord
-module Useq = Useq
-module Prime = Prime
-module Math = Math
-module Fasta = Fasta
+(* general utilities *)
+
+module IO = Io (** quick I/O operations. *)
+
+module Range = Range (** range *)
+
+module Delim = Delim (** CSV Parser *) 
+
+module Coord = Coord (** Coordinates *)
+
+module Useq = Useq (** Utility for Seq module *)
+
+module Prime = Prime (** Prime utilities *)
+
+module Math = Math (** Math functions (especially modular things) *)
+
+module Fasta = Fasta (** FASTA Parser *)
 
 (* For AoC 2020 *)
-module Graph = Graph
-module Block = Block
-module Bitarray = Bitarray
+
+module Graph = Graph (** Graph *)
+
+module Block = Block (** 2D Matrix functor *)
+
+module Bitarray = Bitarray (** Bitarray *)
+
+(** Functor for building Cellular Automata *)
 module CellularAutomata = Cellautomata
+
+(** 2D Matrix Board for CellularAutomata module *)
 module BlockBoard = Blockboard
 
 (* For Aoc 2019 *)
-module IntCode = Intcode
+
+module IntCode = Intcode (** full-featured Intcode VM *)
+
+(** Sparse representation of Block module *)
 module SparseBlock = Sparseblock
 
 (* types *)
-type graph = Graph.t
-type range = Range.t
+
+type graph = Graph.t (** alias of Graph.t *) 
+
+type range = Range.t (** alias of Range.t *)
