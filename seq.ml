55cc5f8461bd87a56f335382f1300310190c3e65
Hyeonung Baek <wiserlix@gmail.com>
2022-03-31 01:03:07 +0900

    Update seq.mli for Ocaml 4.14

--- seq.ml
+++ seq.ml
@@ -5,65 +5,24 @@ include Stdlib.Seq
 
 (* returns nth elements of a sequence, starting from 0.
  * if the length of a sequence is shorter than n, returns None. *)
-let rec nth_opt n seq =
+let rec nth n seq =
   match seq () with
   | Nil -> None
-  | Cons (x, f) -> if n = 0 then Some x else nth_opt (n-1) f
+  | Cons (x, f) -> if n = 0 then Some x else nth (n-1) f
 
-let nth n seq =
-  match nth_opt n seq with
+let nth_opt = nth
+
+let nth_exn n seq =
+  match nth n seq with
   | None -> raise Not_found
   | Some x -> x
 
-(* get length of a sequence. *)
-let length seq =
-  fold_left (fun p _ -> p+1) 0 seq
-
-(* Generators *)
-
-(* init n f returns [ f 0; f 1; ...; f (n-1) ], as a sequence. *)
-let init n f =
-  let rec gen i () =
-    if i = n
-    then Nil
-    else Cons (f i, gen (i+1))
-  in
-  gen 0
-
-(* Taking/Dropping *)
-
-(* take first n elements from a sequence. *)
-let rec take n seq () =
-  if n = 0
-  then Nil
-  else
-    match seq () with
-    | Nil -> Nil
-    | Cons (x, f) -> Cons (x, take (n-1) f)
-
-(* take first n elements from a sequence. *)
-let rec drop n seq =
-  if n = 0
-  then seq
-  else
-    match seq () with
-    | Nil -> empty
-    | Cons (_, f) -> drop (n-1) f
-
-(* take elements from a sequence that meets the predicate. *)
-let rec take_while pred seq () =
-  match seq () with
-  | Nil -> Nil
-  | Cons (x, f) ->
-    if pred x
-    then Cons (x, take_while pred f)
-    else Nil
 
 (* repeat a sequence n times. *)
-let rec repeat n seq () =
+let rec times n seq () =
   if n = 0
   then Nil
-  else append seq (repeat (n-1) seq) ()
+  else append seq (times (n-1) seq) ()
 
 (* repeat every elements in a sequnce n times. *)
 let rec repeat_each n seq () =
@@ -75,10 +34,6 @@ let rec repeat_each n seq () =
   match seq () with
   | Nil -> Nil
   | Cons(x, f) -> append (lo n x) (repeat_each n f) ()
-
-(* repeat a sequence endlessly. *)
-let rec cycle seq =
-  append seq @@ cycle seq
 
 (* accumulate n seq returns a sequence A that
  * A(0) = n
@@ -105,36 +60,15 @@ let rec trail seq () =
   | Nil -> Nil
   | Cons(_, f) -> Cons(seq, trail f)
 
-(* Sequence searching *)
-
-let rec for_all pred seq =
-  match seq () with
-  | Nil -> true
-  | Cons (x, f) ->
-    pred x && for_all pred f
-
-let rec exists pred seq =
-  match seq () with
-  | Nil -> false
-  | Cons (x, f) ->
-    pred x || exists pred f
-
 (* find first elements that meets the predicate from a sequence. *)
-let rec find_opt pred seq =
-  match seq () with
-  | Nil -> None
-  | Cons (x, f) ->
-    if pred x
-    then Some x
-    else find_opt pred f
+let find_opt = find
 
-let find pred seq =
-  match find_opt pred seq with
+let find_exn pred seq =
+  match find pred seq with
   | None -> raise Not_found
   | Some x -> x
 
 (* Structures *)
 
 (* sequence of integers starting from n. *)
-let integer n =
-  unfold (fun n -> Some (n, n+1)) n
+let integer = ints
--- seq.mli
+++ seq.mli
@@ -7,42 +7,29 @@
 
 include module type of Stdlib.Seq
 
-(** [nth_opt n seq] returns Some (nth element of seq). If seq is shorter than
-    n, it returns None. *)
 val nth_opt: int -> 'a t -> 'a option
+[@@deprecated "renamed to nth."]
 
-(** [nth n seq] returns nth element of seq. If seq is shorter than n, it
+(** [nth n seq] returns Some (nth element of seq). If seq is shorter than
+    n, it returns None. *)
+val nth: int -> 'a t -> 'a option
+
+(** [nth_exn n seq] returns nth element of seq. If seq is shorter than n, it
     raises Not_found exception. *)
-val nth: int -> 'a t -> 'a
-
-(** [length seq] returns the length of seq. *)
-val length: 'a t -> int
-
-(** [init n f] is the same as Seq.cons (f 0) @@ Seq.cons (f 1) ...
-    Seq.Nil. *)
-val init: int -> (int -> 'a) -> 'a t
-
-(** [take n seq] returns a sequence that has first n elements of seq. *)
-val take: int -> 'a t -> 'a t
-
-val drop: int -> 'a t -> 'a t
-
-val take_while: ('a -> bool) -> 'a t -> 'a t
+val nth_exn: int -> 'a t -> 'a
 
 (** [repeat_each n seq] returns a sequence that every elements in seq
     repeats n times.*)
 val repeat_each: int -> 'a t -> 'a t
 
-(** [repeat n seq] repeat seq n times. *)
-val repeat: int -> 'a t -> 'a t
-
-(** [cycle seq] cycles seq endlessly. *)
-val cycle: 'a t -> 'a t
+(** [times n seq] repeat seq n times. formerly repeat.*)
+val times: int -> 'a t -> 'a t
 
 (** accumulate n seq returns a sequence A that
     A(0) = n
     A(i) = \sum_{k=0}^{i} A(k).  *)
 val accumulate: int -> int t -> int t
+[@@deprecated "use Stdlib.Seq.scan (+) 0 instead."]
 
 (** [windows n seq] returns a sequence over all contiguous windows of
     length n. The windows overlap. If the slice is shorter than n, the
@@ -56,21 +43,15 @@ val windows: int -> 'a t -> 'a t t
     seq; drop 2 seq; ... ] as a sequence. *)
 val trail: 'a t -> 'a t t
 
-(** [for_all f seq] returns [true] when all elements of seq meet the
- * predicate f, otherwise [false]. If seq is empty, it returns [true]. *)
-val for_all: ('a -> bool) -> 'a t -> bool
-
-(** [exists f seq] returns [true] when an element of seq meets the
- * predicate f, otherwise [false]. If seq is empty, it returns false. *)
-val exists: ('a -> bool) -> 'a t -> bool
-
 (** [find_opt f seq] returns first elements that meets the predicate f.
  * If [exists f seq = false], returns None. *)
 val find_opt: ('a -> bool) -> 'a t -> 'a option
+[@@deprecated "use Seq.find instead."]
 
-(** [find f seq] does the same job as [find_opt], but it raises
+(** [find_exn f seq] does the same job as [find_opt], but it raises
  * Not_found exception when there's no such element. *)
-val find: ('a -> bool) -> 'a t -> 'a
+val find_exn: ('a -> bool) -> 'a t -> 'a
 
 (** increasing integers starting from n. *)
 val integer: int -> int t
+[@@deprecated "use Seq.ints instead."]
