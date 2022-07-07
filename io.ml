8a30673fb87156300c9ffbd646db14b1d319fc07
Hyeonung Baek <wiserlix@gmail.com>
2022-07-07 15:31:20 +0900

    Update IO module for Ocaml 4.14

--- io.ml
+++ io.ml
@@ -1,19 +1,14 @@
 (* read lines from input channel. *)
 let rec input_lines (ic: in_channel) =
-  match input_line ic with
-  | line -> line::(input_lines ic)
-  | exception End_of_file -> []
+  match In_channel.input_line ic with
+  | None -> []
+  | Some line -> line :: input_lines ic
 
-(* NOTE: input_all function will be introduced in Ocaml 4.14. *)
-let input_all (file: in_channel) =
-  really_input_string file (in_channel_length file)
+let input_all =
+  In_channel.input_all
 
 let rec read_lines () =
-  match read_line () with
-  | exception _ -> []
-  | x -> x :: read_lines ()
+  input_lines stdin
 
 let read_all () =
-  Seq.unfold (fun () -> try Some (read_line (), ()) with _ -> None) ()
-  |> List.of_seq
-  |> String.concat "\n"
+  In_channel.input_all stdin
